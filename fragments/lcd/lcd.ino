15e6975d9f36a8ba8f27a5b1f96746b67cc9d35a
MOBILE-OVGFSFC\hayk <hayk.avetisyan@student.howest.be>
2018-03-27 10:29:02 +0200

    LCD Display

---
+++ fragments/lcd/lcd.ino
@@ -0,0 +1,42 @@
+//Written by Nick Koumaris 
+//info@educ8s.tv 
+//educ8s.tv
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
+const int LED = 10;
+
+void setup()  
+{
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  lcd.backlight();
+  lcd.begin(16,2); // Initialize LCD
+  lcd.setCursor(0,0); // Set the cursor at the 4th column and 1st row
+  lcd.print("Pietje Pek!");
+  Serial.write("Hello YouTube!\n");
+  lcd.setCursor(8,1);  // Set the cursor at the 9th column and 2nd row
+  lcd.print("****");
+  Serial.write("****\n");
+  lcd.setCursor(0,2);  // Set the cursor at the 1st column and 3rd row
+  lcd.print("Lets win on Friday!");
+  Serial.write("Thi is a demo text\n");
+  lcd.setCursor(8,3);  // Set the cursor at the 9th column and 4th row
+  lcd.print("****");
+  Serial.write("****\n");
+}
+
+void loop() 
+{ 
+
+}
+
+void switchLedOnFor(int milliseconds) {
+  pinMode(LED, HIGH);
+  delay(milliseconds);
+  pinMode(LED, LOW);
+}
+
