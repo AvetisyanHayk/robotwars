8485dd71c15ea4a1195aa98a02481ae71619a3d1
MOBILE-OVGFSFC\hayk <hayk.avetisyan@student.howest.be>
2018-03-28 11:27:42 +0200

    Servo

---
+++ fragments/servo/servo.ino
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define M3_ANGLE 10
+
+void setup()
+{
+  pinMode(M3_ANGLE, OUTPUT);
+  randomSeed(analogRead(A0));
+  reset();
+}
+
+
+void loop()
+{
+  generator();
+  delay(300);
+}
+
+  void reset(){
+    digitalWrite(13, LOW);
+  }
+  
+  void generator(){
+    // int time = random(1, 5);
+    int time =(analogRead(1)%100)*10+1000;
+    // int time = random(500,6000);
+
+     
+    if( digitalRead(13) == HIGH )      
+      digitalWrite(13, LOW);  
+    else
+        digitalWrite(13, HIGH);
+    
+    delay(time);
+    // delay(time*1000);
+  }
+
+
+
+
+
